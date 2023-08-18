7a12ebf8620022e65f5cd814fe3d90f62b6c1a6a
Piyush Pawar <piyushpawar25@gmail.com>
2023-08-18 16:04:37 +0530

    :tada: feat: initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Root from './routes/root';
+import Tasks from './routes/tasks';
+
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Root />
+	},
+	{
+		path: '/tasks',
+		element: <Tasks />
+	}
+]);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
