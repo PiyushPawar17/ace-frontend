f979bf39abe1c9cf569a015b10c1c7834d90463c
Piyush Pawar <piyushpawar25@gmail.com>
2023-08-27 12:48:50 +0530

    :bug: fix: tasks route access

--- src/routes/tasks/index.tsx
+++ src/routes/tasks/index.tsx
@@ -1,9 +1,24 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import Layout from '@components/Layout';
 import Navbar from '@components/Navbar';
 import List from '@components/List';
 import Tasks from '@components/Tasks';
 
+import { useAuth } from '@utils/context';
+
 const TasksRoute = () => {
+	const { isLoading, isLoggedIn } = useAuth();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!isLoading && !isLoggedIn) {
+			navigate('/');
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
 	return (
 		<main>
 			<Navbar />
